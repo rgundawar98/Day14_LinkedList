f620a389c79e41d6db593ea52229eb8d48cc8a11
rahulgundawar98 <rahulgundawar98@gmail.com>
2023-02-25 20:32:06 +0530

    Create simple linked list UC1_CreateLinkedList

---
+++ LinkedListDataStructure/LinkedListOperation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListDataStructure
+{
+    public class LinkedListOperation
+    {
+        public Node head;
+        public void Insert(int data)
+        {
+            Node node = new Node(data);
+            if(head == null)
+            {
+                head = node;
+                Console.WriteLine("{0} is inserted into linked list",node.data);
+            }
+            else
+            {
+                Node temp = head;
+                while(temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+                Console.WriteLine("{0} is inserted into linked list",node.data);
+            }
+        }
+    }
+}
--- LinkedListDataStructure/Program.cs
+++ LinkedListDataStructure/Program.cs
@@ -7,6 +7,10 @@ namespace LinkedListDataStructure
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Data Structures Linked List");
+            LinkedListOperation  Operation = new LinkedListOperation();
+            Operation.Insert(56);  //UC1 Insert element into linkedlist
+            Operation.Insert(30);
+            Operation.Insert(70);
         }
     }
 }
