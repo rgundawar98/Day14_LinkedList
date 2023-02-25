3847660fe14432dfa28c34f7ee40d92a7e4b981f
rahulgundawar98 <rahulgundawar98@gmail.com>
2023-02-25 20:20:41 +0530

    Adding welcome message

---
+++ LinkedListDataStructure/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinkedListDataStructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Data Structures Linked List");
+        }
+    }
+}
